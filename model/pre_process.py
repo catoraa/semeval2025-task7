1261274e5f04b9b7c93b8299443d64c2244942da
myh <1164726499@qq.com>
2024-11-14 13:57:45 +0800

    -

--- model/pre_process.py
+++ model/pre_process.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 parse_col = lambda s: ast.literal_eval(s.replace('\n', '\\n')) if s else s
 
-fact_checks = pd.read_csv('../test_data/fact_checks.csv')
-posts = pd.read_csv('../test_data/posts.csv')
+fact_checks = pd.read_csv('../sample_data/trial_fact_checks.csv')
+posts = pd.read_csv('../sample_data/trial_posts.csv')
 
 #fact check预处理
 fact_checks = fact_checks.fillna('').set_index('fact_check_id')
@@ -13,7 +13,7 @@ for col in ['claim', 'instances', 'title']:
 fact_checks['fact_check_text'] = fact_checks['claim'].apply(lambda x: str(x[1]) if x else '') + ' ' + fact_checks[
     'title'].apply(lambda x: str(x[1]) if x else '')
 fact_checks = fact_checks['fact_check_text']
-fact_checks.to_csv('../preprocess_test/fact_checks_pre.csv', index=True)
+fact_checks.to_csv('../preprocess_test/trial_fact_checks_pre.csv', index=True)
 
 # post预处理
 posts = posts.fillna('').set_index('post_id')
@@ -22,4 +22,4 @@ for col in ['instances', 'ocr', 'verdicts', 'text']:
 posts['post_text'] = posts['verdicts'].apply(lambda x: str(x[0]) if x else '') + ' ' + posts['ocr'].apply(
     lambda x: str(x[0][1]) if x else '') + ' ' + posts['text'].apply(lambda x: str(x[1]) if x else '')
 posts = posts['post_text']
-posts.to_csv('../preprocess_test/posts_pre.csv', index=True)
+posts.to_csv('../preprocess_test/trial_posts_pre.csv', index=True)
